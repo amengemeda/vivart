ff2491e4de36bb4b4e552f282338cfd2933f0a89
Amen Gemeda <amentamene@gmail.com>
2021-03-18 06:27:30 +0300

    Bugs in findArtist page  are fixed

--- Js/artistProfile.js
+++ Js/artistProfile.js
@@ -2,12 +2,17 @@ $(document).ready(function () {
     var small={width: "240px",height: "205px"};
     var large={width: "400px",height: "382px"};
     var count=2; 
-    $("#img").css(small).on('click',function () { 
+    $(".img").css(small).on('click',function () { 
         $(this).animate((count==2)?large:small);
         count = 2-count;
+    });
+    $(".video").css(small).on('click',function () { 
+        $(this).animate((count==2)?large:small);
+        count = 2-count;
+        console.log("called");
     });
 });
 
 document.getElementById("back").onclick = function () {
-        location.href = "findArtist.php";
+        window.history.back();
     };
